1b668ff167d10b019088436ec7878503225b0bbe
Mahima Gupta <mahimagupta1195@gmail.com>
2019-07-21 17:45:02 -0700

    Circular Array with comments

--- Circular Array.java
+++ Circular Array.java
@@ -3,36 +3,73 @@
 // Did this code successfully run on Leetcode : Yes
 
 public class Solution {
+    int len;
+    /**
+     * Moves the pointer 'i' ahead one iteration.
+     */
+    private int advance(int[] nums, int i) {
+        i += nums[i];
+        if (i < 0) i += len;
+        else if (i > len - 1) i %= len;
+        return i;
+    }
+    
     public boolean circularArrayLoop(int[] nums) {
-        boolean found = false;
-
-        for ( int n=0; n<nums.length; n++ ) {
-            Integer ps = n;
-            Integer pf = next(nums, 0, n);
-            int dir = nums[n];
-
-            while ( ps != null && pf != null && ps != pf ) {
-                ps = next(nums, dir, ps); //slow gets the next value
-                pf = next(nums, dir, next(nums, dir, pf)); //fast gets the value after next
+        // Handle bad input
+        if (nums == null || nums.length < 2) return false;
+        
+        len = nums.length;
+        
+        /**
+         * Check every possible start location.
+         * We may start at a short-loop, for instance, but the Array
+         * may still contain a valid loop.
+         */
+        for (int i = 0; i < len; i++) {
+            /**
+             * We set elements to 0 which are on known non-loop paths.
+             * So, if we encounter a 0, we know we're not on a loop path.
+             * So, move to the next start location in the list.
+             */
+            if (nums[i] == 0) continue;
+            
+            // Stagger our starts, so we don't conclude we've found a loop,
+            // as we might otherwise when slow == fast.
+            int slow = i, fast = advance(nums, slow);
+            
+            /** 
+             * Whether i is positive or negative defines our direction, so if
+             * the directions differ, so too will the signs.
+             * If the signs differ, we can't be in a 'forward' or a 'backward'
+             * loop, so we exit the traverse.
+             */
+            while (nums[i] * nums[fast] > 0 &&
+                    nums[i] * nums[advance(nums, fast)] > 0) {
+                if (slow == fast) {
+                    if (slow == advance(nums, slow)) break; // 1-element loop
+                    return true;
+                }
+                slow = advance(nums, slow);
+                fast = advance(nums, advance(nums, fast));
             }
-
-            if ( ps != null && ps == pf ) { //when the two pointers meet, circular array is present
-                found = true;
-                break;
+            
+            /**
+             * If we're here, we didn't find a loop, so we know this path
+             * doesn't have a loop, so we re-traverse it until we reverse
+             * direction or encounter a '0' element.
+             * During the re-traverse, we set each element we see to 0.
+             */
+            slow = i;
+            int sgn = nums[i];
+            while (sgn * nums[slow] > 0) {
+                int tmp = advance(nums, slow);
+                nums[slow] = 0;
+                slow = tmp;
             }
         }
-
-        return found;
-    }
-
-    Integer next(int[] nums, int dir, Integer pos) {
-        if ( pos == null ) return null; // null, return null
-        if ( dir * nums[pos] < 0 ) return null; // change in direction, return null
-
-        Integer next = (pos + nums[pos]) % nums.length;
-        if ( next < 0 ) next += nums.length; // wrap negative
-
-        if ( next == pos ) next = null; // self-pointer, return null
-        return next;
+        
+        // We've tested the whole array and have not found a loop,
+        // therefore there isn't one, so return false.
+        return false;
     }
 }
