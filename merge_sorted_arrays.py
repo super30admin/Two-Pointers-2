50e3bfe4561f521462843dd3d546286264690482
88sanjay <88.sanjay@gmail.com>
2019-10-29 21:11:28 -0400

    Create merge_sorted_arrays.py

---
+++ merge_sorted_arrays.py
@@ -0,0 +1,32 @@
+"""
+Time complexity O(M+N)
+Space complexity O(1)
+
+Runs on leet code
+"""
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        
+        p1 = m -1
+        p2 = n -1 
+        
+        while (p1 >= 0) and (p2 >= 0) :
+            if nums2[p2] >= nums1[p1] :
+                nums1[p1+p2+1] = nums2[p2]
+                p2 -=1
+            else :
+                nums1[p1+p2+1] = nums1[p1]
+                p1 -=1
+        
+        while p2 >= 0 :
+            nums1[p1+p2+1] = nums2[p2]
+            p2 -= 1
+        
+        return nums1
