15990cef2492dc52ccfb6613dbfd6d6535f9591e
Rishika Machina <39758648+RishikaMachina@users.noreply.github.com>
2019-12-05 01:39:36 -0800

    Update Problem_1.py

--- Problem_1.py
+++ Problem_1.py
@@ -2,6 +2,16 @@
 # Time complexity :  O(n)
 # Memory complexity : O(1)
 
+
+'''
+1) Initialized pointer first which travels in range [1,len(nums)-1). 1 is inclusive boundary and len(nums)-1 is exclusive.
+
+2) If the numbers at first-1 and first+1 indexes are equal, number at first pointer is deleted.
+
+3) If not equal we increment first pointer
+
+'''
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         if len(nums) < 3:
