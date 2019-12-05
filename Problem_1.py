92be25ad79cca2b88b851eea2e096f6a4c4161bc
Rishika Machina <39758648+RishikaMachina@users.noreply.github.com>
2019-12-04 23:53:04 -0800

    Completed Problem_1.py

---
+++ Problem_1.py
@@ -0,0 +1,17 @@
+# Runs on Leetcode
+# Run time complexity : O(nlogn)
+# Memory complexity : O(m+n)
+
+# Will submit one more solution after clas
+
+'''
+
+1) Add second array into first array from index m
+2) Sort them using in built sort function
+
+'''
+
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        nums1[m:] = nums
+        nums1.sort()
