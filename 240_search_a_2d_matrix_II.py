26bffe3eb552a12a3270f33027730596109e19b5
Nikhil Agrawal <nikrasiya@gmail.com>
2020-01-10 06:51:30 -0500

    merge_sorted_array

---
+++ 240_search_a_2d_matrix_II.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """
+            https://leetcode.com/problems/search-a-2d-matrix-ii/
+            // Time Complexity :
+            // Space Complexity :
+            // Did this code successfully run on Leetcode :
+            // Three line explanation of solution in plain english :
+        """
+        pass
+
+
+if __name__ == '__main__':
+    h = Solution()
+    print(h.searchMatrix([
+        [1, 4, 7, 11, 15],
+        [2, 5, 8, 12, 19],
+        [3, 6, 9, 16, 22],
+        [10, 13, 14, 17, 24],
+        [18, 21, 23, 26, 30]
+    ]))
---
+++ 80_remove_duplicates_from_sorted_array_II.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """
+            // Time Complexity :
+            // Space Complexity :
+            // Did this code successfully run on Leetcode :
+            // Three line explanation of solution in plain english :
+        """
+        pass
+
+
+if __name__ == '__main__':
+    h = Solution()
+    print(h.removeDuplicates([1, 1, 1, 2, 2, 3]))
+    print(h.removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]))
