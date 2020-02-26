980b401e188bfbc95d61fe03f6bc44008db46502
Payel Bandyopadhyay <payelbandyopadhyay@Payels-MacBook-Air.local>
2020-02-25 17:45:00 -0800

    after class solutions

--- merge_arrays.java
+++ merge_arrays.java
@@ -1,10 +1,40 @@
 
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(M+N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
 // Three line explanation of solution in plain english
+//Find  last index of both the arrays.
+//Start comparing the elements from boht the arrays from the end.
+//Then put the new elemnt in first+second+1 index.
+//DO next while loop to iterate and place second array elements in first.
+
 
 // Your code here along with comments explaining your approach
-//A brute force approach could be iterate over longer array, then the shorted array.
-//Then for each element in longer array compare it will all the elements of shorter.
-//
+
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        //find last index of both the arrays
+        int first = m-1, second = n-1;
+        
+        //loop to compare elements from both ends
+        while(first >= 0 && second >= 0){
+        	//element in first array is greater
+            if(nums1[first] > nums2[second]){
+                nums1[first+second+1] = nums1[first];
+                first--;
+            }else{ // element in second array is greater
+                nums1[first+second+1] = nums2[second];
+                second--;
+            }
+        }
+        
+
+        //loop to put second arrays lements in first, incase first is empty and second is not
+        while(second >= 0){
+                nums1[second] = nums2[second];
+                second--;
+        }
+    }
+}
--- search_2Darray.java
+++ search_2Darray.java
@@ -1,8 +1,35 @@
 
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(M+N)  this is the maximum number of elements we traverse in worst case
+// Space Complexity : O(1) no extra space used
+// Did this code successfully run on Leetcode : Yes
 // Three line explanation of solution in plain english
 
 // Your code here along with comments explaining your approach
-//Brute force, for each row search the target element.
+
+//initialize two pointers which are row and col in this case. We start from bottom left.
+//loop the 2D matrix to find the target
+// if target value found return true
+// if target is greater than current value move right, increment col.
+// else if target is lesser then current value, move up and decrement row.
+// finally return false
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        //initialize two pointers which are row and col in this case. We start from bottom left.
+        int row= matrix.length-1, col= 0;
+        
+        //loop the 2D matrix to find the target
+        while(row>=0 && col< matrix[0].length){
+            if(target == matrix[row][col]){ // if target value found return true
+                return true;
+            }else if(target > matrix[row][col]){ // if target is greater than current value move right, increment col.
+                col++;
+            }else{ // else if target is lesser then current value, move up and decrement row.
+               row--; 
+            }
+        }
+        
+        return false;
+    }
+}
