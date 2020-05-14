56db14348f945a8dd1fc128daf7172e4f1399186
Siyami Avci <siyamiavci@gmail.com>
2020-05-14 04:28:35 -0700

    fix

--- p1.js
+++ p1.js
@@ -1,13 +1,6 @@
 // ## Problem1 
 // (https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/)
 
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-
-// ## Problem1 
-// (https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/)
-
 // Time Complexity : O(n)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
