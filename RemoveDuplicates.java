4cf62bc65f2de212079b4d5b27741e7d02d8d287
Shivani Reddy <shivanireddy010197@gmail.com>
2020-06-10 21:53:50 -0700

    Completed

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -2,7 +2,9 @@
 // Time Complexity : O(n)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
-// Three line explanation of solution in plain english
+
+// Apprach: We maintain a pointer for iterating through array upto n-2 and
+// another for replacing the number if it is present more than twice.
 
 // Your code here along with comments explaining your approach
 
