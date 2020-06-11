04247e2ab8ef398e470fea3f982d0d9b85add1f7
Sumukh Ballal <sumukh.ballal@appdynamics.com>
2020-06-10 21:41:59 -0700

    AUTO

--- removeDuplicates.java
+++ removeDuplicates.java
@@ -1,3 +1,10 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Three line explanation of solution in plain english
+
+// Your code here along with comments explaining your approach
+
 class Solution {
     public int removeDuplicates(int[] nums) {
        if (nums.length<3)
--- search2dmatrix.java
+++ search2dmatrix.java
@@ -1,3 +1,10 @@
+// Time Complexity : O(m+n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Three line explanation of solution in plain english
+
+// Your code here along with comments explaining your approach
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
