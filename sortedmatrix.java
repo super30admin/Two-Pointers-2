2a4a97d755fb7f393f974c1d5cccbe753eba7767
Gahan Jagadeesh <gahanjagadeesh@gmail.com>
2020-06-10 22:41:48 -0400

    Completed 2 problems

---
+++ sortedmatrix.java
@@ -0,0 +1,24 @@
+//Time complexity: O(n+m)
+//Space complexity:O(1)
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix==null || matrix.length==0) return false;
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int i=m-1;
+        int j=0;
+        while(i>=0 && j<n){
+            if(matrix[i][j]==target){
+                return true;
+            }
+            else if(matrix[i][j]<target){
+                j++;
+            }
+            else{
+                i--;
+            }
+        }
+        return false;
+    }
+}
