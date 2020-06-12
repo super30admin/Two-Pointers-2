95f562ae9278540e0beda723bcce0c0b23be120a
Ashish <gurav.ashish1@gmail.com>
2020-06-12 01:20:21 -0500

    merge sort after class submission

--- MergeArray.java
+++ MergeArray.java
@@ -8,3 +8,35 @@ class Solution {
     Arrays.sort(nums1);
 }
 }
+//----------------3 pointer solution as shown in class-----------------
+// Time Complexity : O(n)
+//Space Complexity : O(1) 
+//Did this code successfully run on Leetcode : Yes
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+  
+        int i = (m + n) - 1;
+        int j  = m - 1;
+        int k = n - 1;
+        
+        while(j >= 0 && k >= 0){
+            if(nums2[k]>nums1[j]){
+                nums1[i] = nums2[k];
+                i--;
+                k--;
+            }
+            else{
+                nums1[i] = nums1[j];
+                j--;
+                i--;
+            }
+        }
+        
+        while(k >= 0){
+            nums1[i] = nums2[k];
+            k--;
+            i--;
+        }
+}
+}
