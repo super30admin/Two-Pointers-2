187e234b6727522f3ad38a6eecc3f9f690dbac3c
karthikprabhu alagu <mail2karthi07@gmail.com>
2020-06-11 23:10:42 -0400

    Two-Pointers-2: Search matrix.

---
+++ SearchMatrix.swift
@@ -0,0 +1,29 @@
+// Time Complexity : (O)m+n
+// Space Complexity : (O)1
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : yes failed for test case [[-5]]-10, [[1,1]] 2. mistakenly taken m = matrix[0].count and n = matrix.count
+//
+class Solution {
+    func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+       //edge
+        if matrix.count == 0 {
+            return false
+        }
+        var n = matrix[0].count
+        var m = matrix.count
+        
+        var r = 0
+        var c = n - 1
+        while c >= 0 && r < m {
+            if matrix[r][c] == target {
+                return true
+            } else if matrix[r][c] > target {
+                c -= 1
+            } else {
+                r += 1
+            }
+        }
+        
+        return false
+    }
+}
