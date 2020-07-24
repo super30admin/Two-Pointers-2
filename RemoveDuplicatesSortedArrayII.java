395d8c9930d46aba6b3824b506a3f07ca2d46656
Arpit Sharma <arpit317@gmail.com>
2020-07-24 15:25:56 -0700

    Create RemoveDuplicatesSortedArrayII.java

---
+++ RemoveDuplicatesSortedArrayII.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int slow = 1;
+        int fast = 1;
+        int prev = 0;
+        int flag = 0;
+        
+        while(fast < nums.length){
+            if(nums[fast] == nums[prev]){
+                if(flag == 0){
+                    nums[slow] = nums[fast];
+                    slow++;
+                    fast++;
+                    flag++;
+                }
+                else{
+                    fast++;
+                }
+            }
+            else{
+                prev = fast;
+                nums[slow] = nums[fast];
+                slow++;
+                fast++;
+                flag = 0;
+            }
+            
+        }
+        return slow;
+    }
+}
