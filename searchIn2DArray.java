dae1c0ac70cd17d8387155340d6f949d2679c34d
Akshay Kajale <aksh.kajale@gmail.com>
2020-10-01 13:15:50 -0700

    Update searchIn2DArray.java

--- searchIn2DArray.java
+++ searchIn2DArray.java
@@ -1,5 +1,5 @@
 
-// Time Complexity : O(n)
+// Time Complexity : O(m*n)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Three line explanation of solution in plain english
