5350aa9078a123c310b19a6376a4c8dac1518d84
shubhammanoharsasane <shubhs.ss4@gmail.com>
2020-10-29 02:36:56 -0400

    Two pointers-2

--- Sample.java
+++ Sample.java
@@ -1,7 +1,20 @@
-
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Three line explanation of solution in plain english
+// Time Complexity : O(n3)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes (brute force solution)
+// going through the all elements we check for the target
 
 // Your code here along with comments explaining your approach
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
