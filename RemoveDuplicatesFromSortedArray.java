27a2c3075d81c88370598dc73f647d70686da842
harsh <harshsaini697@gmail.com>
2020-11-01 21:00:16 -0600

    t3

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -6,7 +6,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
-        //int i = 1;
         int j = 1;
         int count = 1;
         for(int i = 1;i < nums.length; i++){
