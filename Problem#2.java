5d656234004265fe68992a983a182809e5e65c66
Deepali Patil <deepalipatil24@yahoo.com>
2020-12-05 03:29:36 -0800

    Added Two-Pointers-2 PRs

---
+++ Problem#2.java
@@ -0,0 +1,41 @@
+// 88. Merge Sorted Array ( PR#36 S30)
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        if(nums1 == null || nums1.length == 0)
+            return;
+        
+        int i = m+ n -1, p1 = m -1, p2 = n-1;
+        while(p1>=0 && p2 >= 0) {
+            
+            if(nums1[p1] > nums2[p2]){
+                
+                nums1[i] = nums1[p1];
+                p1--;
+            }
+            else {
+                nums1[i] = nums2[p2] ;
+                p2--;
+                
+            }
+            i--;
+            }
+        
+        while(p2 >=0){
+            nums1[i] = nums2[p2];
+                            p2--;
+                            i--;
+        }
+        }
+        
+    }
+
+/*
+Complexity Analysis
+
+Time complexity : 
+O(n+m).
+Space complexity : 
+O(1).
+*/
