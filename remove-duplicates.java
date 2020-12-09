577e6c4d6d1eb59f358e05096e86b1715f6be592
Rishi Raj <rishiraj824@gmail.com>
2020-12-09 02:55:02 -0500

    Create remove-duplicates.java

---
+++ remove-duplicates.java
@@ -0,0 +1,25 @@
+// Time - O(N)
+// Space - O(1)
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int j = 1;
+        int count = 1;
+        for(int i=1;i<nums.length;i++) {
+            if(nums[i] == nums[i-1]) {
+                count++;
+            }
+            else {
+                count = 1;
+            }
+            if(count <= 2) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
