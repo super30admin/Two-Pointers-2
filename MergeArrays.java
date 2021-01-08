5a2ff957b513ba0279557797ee2f5caf28211f9c
gangakalburgi <56897430+gangakalburgi@users.noreply.github.com>
2021-01-07 18:30:39 -0800

    pointer-2, completed only 1 problem

---
+++ MergeArrays.java
@@ -0,0 +1,31 @@
+class MergeArrays {
+    public void merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+        int k = nums1.length-1;
+        
+        int i = m-1;
+        int j = n-1;
+        
+        while(i >= 0 && j >= 0)
+        {
+            if(nums2[j] > nums1[i])
+            {
+                nums1[k--] = nums2[j--];
+            }
+            
+            else 
+            {
+                nums1[k--] = nums1[i--];
+            }
+            
+            
+        }
+        while(j >= 0)
+        {
+            nums1[k--] = nums2[j--];
+        }
+        
+        
+       ß
+    }
+}
