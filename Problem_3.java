5846a6439a0e92f4084a465f149a7c81817f12b8
Ashish Mishra <ashishk861@gmail.com>
2021-01-14 19:23:23 -0500

    Update Problem_3.java

--- Problem_3.java
+++ Problem_3.java
@@ -4,19 +4,39 @@
 // Any problem you faced while coding this :
 
 
-// Your code here along with comments explaining your approach
+// This is the brute force approach.
 class Solution {
-    public int maxArea(int[] height) {
-        int maxValue=0;
-        int min=0;
-        for(int i=0;i<height.length;i++){
-            for(int j=i+1;j<height.length;j++){
-                min=Math.min(height[i],height[j]);
-                if((min*(j-i))>maxValue){
-                    maxValue=min*(j-i);
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==target){
+                    return true;
                 }
             }
         }
-        return maxValue;
+        
+        return false;
+    }
+}
+
+// Time Complexity :O(m*n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i=0;
+        int j=matrix[0].length-1;
+        while(i<matrix.length && j>=0){
+            if(target<matrix[i][j]){
+                j=j-1;
+            }else if(target>matrix[i][j]){
+                i=i+1;
+            }else if(target==matrix[i][j]){
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
