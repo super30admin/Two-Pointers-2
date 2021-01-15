8bfbc8ca13d2b1a0cbc2f4e95816caa46829c8f6
Ashish Mishra <ashishk861@gmail.com>
2021-01-14 19:36:17 -0500

    Update Problem_2.java

--- Problem_2.java
+++ Problem_2.java
@@ -13,20 +13,8 @@ class Solution {
             nums1[0]=nums2[0];
             return;
         }
-        int start=0;
-        for(int i=nums1.length-1;i>=0;i--){
-            if(nums1[i]==0){
-                start=i;
-                //break;
-            }else{
-                break;
-            }
-        }
-        //start=start+1;
-        if(start>nums1.length){
-            nums1[0]=nums2[0];
-            return;
-        }
+        int start=m;
+
         
         for(int i=0;i<nums2.length;i++){
             nums1[start]=nums2[i];
@@ -37,12 +25,35 @@ class Solution {
     }
 }
 
-// Time Complexity :O(nlogn)
-// Space Complexity :O(n)
+// Time Complexity :O(n)
+// Space Complexity :O(1)
 // Did this code successfully run on Leetcode :Yes
 // Any problem you faced while coding this :
 
 
 // In this we use the three pointer approach.
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(nums1==null || nums1.length==0) return;
+        int pointer1=m-1;
+        int pointer2=n-1;
+        int index=m+n-1;
+        while(pointer1>=0 && pointer2 >=0){
+            if(nums1[pointer1]>nums2[pointer2]){
+                nums1[index]=nums1[pointer1];
+                pointer1=pointer1-1;
+            }else{
+                nums1[index]=nums2[pointer2];
+                pointer2=pointer2-1;
+            }
+            index=index-1;
+        }
+        while(pointer2>=0){
+            nums1[index]=nums2[pointer2];
+            pointer2=pointer2-1;
+            index=index-1;
+        }
+    }
+}
 
 
