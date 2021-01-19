7928a338813502f7e01edaab546270464b77e4bb
yahska-ednihs <75634279+yahska-ednihs@users.noreply.github.com>
2021-01-18 20:04:51 -0800

    Adding removing duplicates solution

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,6 +1,43 @@
-# Time Complexity :
-# Space Complexity :
-# Did this code successfully run on Leetcode : No
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
 # Three line explanation of solution in plain english
-# I am still not able to solve this problem. I will have to check the video again
-# Your code here along with comments explaining your approach
+# I have two pointers, p1 and p2, p2 moves ahead and finds the correct elements and copies them to p1
+# Once p2 goes out of array we return p1
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left = 1
+        right = 1
+        length = len(nums)
+        count = 1
+        current = nums[0]
+        k=2
+        
+        while right < length:
+            nums[left] = nums[right]
+            if nums[left] == current:
+                count += 1
+            else:
+                current = nums[left]
+                count = 1
+                
+            
+            if count > k:
+                while right < length and nums[right] == current:
+                    right += 1
+                
+                if right < length:
+                    nums[left] = nums[right]
+                    current = nums[right]
+                    count = 1
+                else:
+                    return left
+                
+                left+=1
+                right+=1
+            else:
+                left+=1
+                right+=1
+                
+        return left
