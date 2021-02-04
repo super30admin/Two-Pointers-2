b42ae42e064e5123b3e98625b428423e0f25d273
akashtanwani <77640435+akashtanwani@users.noreply.github.com>
2021-02-04 12:03:59 -0600

    Completed Problem1

--- problem1.py
+++ problem1.py
@@ -5,3 +5,15 @@
 
 
 // Your code here along with comments explaining your approach
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        count=j=1
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1]:                          //check for duplicate values
+                count+=1
+            else:
+                count=1
+            if count<=2:                                  //if element has 2 or less duplicates then we can assign the value at index i to value at index j because j is either pointing to the index i or it is pointing to the extra duplicate
+                nums[j]=nums[i]
+                j+=1
+        return j
