4def6a30644a2876e95631c0c01128a67c42cd56
Ruchika Hazariwal <ruchika.hazariwal@sjsu.edu>
2021-04-05 15:22:21 -0700

    Create 30_remove_duplicates_from_the_sorted_array_2.java

---
+++ 30_remove_duplicates_from_the_sorted_array_2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length==0)
+            return 0;
+        
+        int slow = 1, fast = 1;
+        int count = 1;
+        
+        while(fast<nums.length){
+            if(nums[fast]==nums[fast-1]){
+                count++;
+            }
+            else{
+                count=1;
+            }
+            if(count<=2){
+                nums[slow] = nums[fast];
+                slow++;
+            }  
+            fast++;
+        }
+        return slow;
+    }
+}
+
+//TC => O(n)
+//SC => O(1)
