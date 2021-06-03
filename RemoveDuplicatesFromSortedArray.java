ddb6ee94788683623c96018558aadead049b9c07
Deepikasri5 <deepikasrigr.1995@gmail.com>
2021-06-03 00:12:36 -0400

    Two-Pointers-2

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,28 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+//use fast and slow pointers and check if count is less than 2 then
+//swap i and jth elements
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        int j = 1;
+        int count = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i-1]){
+                count++;
+            }
+            else{
+                count = 1;
+            }
+            if(count <= 2){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+
+    }
+} 
