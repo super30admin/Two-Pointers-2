957e26dfb1e078f12a6c764cbd7acf3670239550
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-06-11 12:07:32 -0700

    Update mergeSorted.cpp

--- mergeSorted.cpp
+++ mergeSorted.cpp
@@ -2,36 +2,28 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        int i=nums2.size()-1;
-        int j=nums1.size()-1;
-            int k = nums1.size()/2 -1;
-        
-        while(k>=0 and i>=0)
+      int i=m-1,j=n-1,k=n+m-1;
+        while(i>=0 && j>=0)
         {
-            if(nums2[i]>nums1[k])
+            if(nums1[i]<=nums2[j])
             {
-                nums1[j]=nums2[i];
-               i--;
+                nums1[k]=nums2[j];
                 j--;
-            }
-            else if(nums1[k]>nums2[i]){
-                nums1[j]=nums1[k];
                 k--;
-                j--;
-                
             }
-            
+            else
+            {
+                nums1[k]=nums1[i];
+                i--;
+                k--;
+            }
         }
-        while(k>0)
+        while(j>=0)
         {
-            nums1[j]=nums1[k];
+            nums1[k]=nums2[j];
+            j--;
             k--;
         }
-        while(i>0)
-        {
-            nums1[j]=nums2[i];
-            i--;
-        }
-        
+   
     }
 };
