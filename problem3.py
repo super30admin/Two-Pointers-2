48d9231d7adc774849a0147bc78b29ad5b8902c0
sahil1291sharma <sahil1291sharma@users.noreply.github.com>
2021-06-11 03:32:43 -0400

    update to 2 d array problem

--- problem3.py
+++ problem3.py
@@ -12,3 +12,22 @@ class Solution:
 #Brute force solution with O(m*n) time complexity
         
  
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        i, j = 0, n-1
+        
+        while i<m and j >=0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                j -=1
+            else:
+                i +=1
+        
+        return False
+    
+# Pointer solution with O(m+n) time complexity
+# Space complexity is O(1)
