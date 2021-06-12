ff5db55055a6ae1cd856687185015e24abdd1a4a
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-06-11 22:30:34 -0700

    Update duplicates.cpp

--- duplicates.cpp
+++ duplicates.cpp
@@ -1,1 +1,18 @@
 //Remove duplicates
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+       int i=0;
+        for(int j=1;j<nums.size();j++)
+        {
+            if(nums[i]!=nums[j])
+            {
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+    //time complexity: O(N)
+    //space complexity: O(1)
+};
