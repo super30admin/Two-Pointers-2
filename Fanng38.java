9955b3e38356a81cfa582759d721c35f515092ec
CINTOANTO <63736772+CINTOANTO@users.noreply.github.com>
2021-06-15 02:10:43 +0530

    completed pointers-2

---
+++ Fanng38.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class Fanng38 {
+
+    public boolean searchMatrix(int[][] matrix, int target){
+        int r=0;int c=matrix[0].length-1;
+        while(c>=0 && r<matrix.length){
+            if(matrix[r][c]==target){
+                return true;
+            }else if(matrix[r][c]>target){
+                c--;
+            }else{
+                r++;
+            }
+        }
+        return false;
+    }
+}
