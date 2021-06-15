921d00b3706f93aa18d7d31bf2a0f6f2fb8a25f7
Dhiren Paryani <dhirenparyani@Dhirens-MacBook-Pro.local>
2021-06-14 17:16:29 -0700

    Completed Two-Pointer-2

--- SearchMatrix.java
+++ SearchMatrix.java
@@ -1,3 +1,15 @@
+// Time Complexity : O(m+n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Three line explanation of solution in plain english
+
+// Your code here along with comments explaining your approach
+/*
+ We can either start from right top corner or left bottom corner since we can take decisions from both the ends to either move up/down or left/right
+ if we are starting form top right corner then if target is smaller than the current element we go left if target is greater than we go down.
+*/
+
+
 public class SearchMatrix {
     public static boolean searchMatrix(int[][] matrix, int target) {
         
