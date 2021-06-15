1b6012097ff3567fe16dbc08269696529ccd3883
AdithyaHolla <56132230+AdithyaHolla@users.noreply.github.com>
2021-06-15 10:01:43 -0700

    Create Problem_1.py

---
+++ Problem_1.py
@@ -0,0 +1,27 @@
+"""
+Time Complexity : O(n)
+Space Complexity : O(1)
+    
+"""
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums or len(nums) == 0:
+            return 0
+        
+        k = 2 
+        count = 1
+        j = 1 
+        
+        for i in range(1,len(nums)):
+            
+            if nums[i] == nums[i-1]:
+                count += 1 
+            else:
+                count = 1 
+            
+            if count <= k:
+                nums[j] = nums[i]
+                j += 1 
+        
+        return j
