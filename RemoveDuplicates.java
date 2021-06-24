2a0f506581189e9ea254dc357b55877dbe01ff8d
Kunj-97 <57332203+Kunj-97@users.noreply.github.com>
2021-06-23 22:43:50 -0400

    Update RemoveDuplicates.java

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -4,11 +4,6 @@ Time Complexity: O(n) where n is the size of the array.
 Space Complexity : O(1)
 Did it run on Leetcode: Yes
 
-Approach: 3 pointers.
-1. We keep three pointers, previous, next and indicator. Initially, previous points to 0, next and indicator to 1.
-2. We keep track of the duplicates(if more than 2) using a flag. 
-3. If we find more than two duplicates, we move the next element to the current position(Indicator). Here indicator makes sure that the elements to the left of it are proper.(just 2 duplicates, if any)
-4. In the end, we return the length of the indicator, because it's index gives us the length of the already properly positioned elements.
 */
 
 class Solution {
