1469561b6eccb6ce38bb8e860ea70c2c6ba89405
Kunj-97 <57332203+Kunj-97@users.noreply.github.com>
2021-06-23 22:44:20 -0400

    Update SearchMatrix.java

--- SearchMatrix.java
+++ SearchMatrix.java
@@ -2,13 +2,8 @@
 Problem: Search in a sorted matrix.
 Time Complexity: O(m+n) where m is the number of rows, and n is the number of columns of the given matrix.
 Space Complexity : O(1)
-Did it run on Leetcode: Yes
 
-Approach:
-1. We first keep a check on the element present in the last column of the first row.
-2. If it is equal to the target, we return true.
-3. However, if the target is less than that element, we move left(because we know the matrix is sorted row-wise and column wise)
-4. If the target is more than that element, we move downward. We keep repeating this until we try to find the element. We return false, otherwise.
+
 */
 
 
