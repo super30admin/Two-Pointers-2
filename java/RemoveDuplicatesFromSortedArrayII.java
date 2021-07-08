4800947a6adc6856e269b1dcfd98c25721fb888f
jisakp7 <kavya.pathi@jbhunt.com>
2021-07-08 18:24:29 -0500

    RemoveDuplicatesFromSortedArrayII.java

---
+++ java/RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,51 @@
+
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Three line explanation of solution in plain english
+/*
+using 3 pointers to do inplace operations and to replace the 3rd repititive element with next unique one
+ */
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        //
+        // Initialize the counter and the second pointer.
+        //
+        int j = 1, count = 1;
+
+        //
+        // Start from the second element of the array and process
+        // elements one by one.
+        //
+        for (int i = 1; i < nums.length; i++) {
+
+            //
+            // If the current element is a duplicate, increment the count.
+            //
+            if (nums[i] == nums[i - 1]) {
+
+                count++;
+
+            } else {
+
+                //
+                // Reset the count since we encountered a different element
+                // than the previous one.
+                //
+                count = 1;
+            }
+
+            //
+            // For a count <= 2, we copy the element over thus
+            // overwriting the element at index "j" in the array
+            //
+            if (count <= 2) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+}
