10d9a5155fe9bdd7a87b8669211d008100901346
Durgesh Patil <patildurgesh1998@gmail.com>
2021-08-07 20:25:03 -0400

    Update Problem-2.cpp

--- Problem-2.cpp
+++ Problem-2.cpp
@@ -7,23 +7,11 @@ public:
         while(i>=0 && j>=0)
         {
             if(nums1[i]<nums2[j])
-            {
-                nums1[k]=nums2[j];
-                j--;
-                k--;
-            }
+                nums1[k--]=nums2[j--];
             else
-            {
-                nums1[k]=nums1[i];
-                i--;
-                k--;
-            }
+                nums1[k--]=nums1[i--];
         }
         while(j>=0)
-        {
-            nums1[k]=nums2[j];
-            k--;
-            j--;
-        }
+            nums1[k--]=nums2[j--];
     }
 };
