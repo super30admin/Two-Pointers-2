33aaca16ddbcc6e766f33d9eaa770221f3679ea7
Aparna Dalal <34259886+aparnada7@users.noreply.github.com>
2021-08-12 19:36:57 -0500

    Two pointers 2 submission
    Two pointers 2 submission

---
+++ Problem#3_SearchIn2DMatrix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix==null || matrix.length ==0){
+            return false;
+        }
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int i = 0;
+        int j = n-1;
+        //Row decrease going ahead && col increase
+        while(i<m && j>=0){
+            
+        }
+    }
+}
