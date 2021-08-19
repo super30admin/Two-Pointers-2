d0b721faba092ce9282e7f6de2ee73c8d3592f70
Srinivasa Rao Yedla <ysrinu15@gmail.com>
2021-08-18 23:42:24 -0700

    Two-Pointers-2

---
+++ RemoveDuplicatesFromArray2.java
@@ -0,0 +1,32 @@
+public class RemoveDuplicatesFromArray2 {
+        public int removeDuplicates(int[] nums) {
+    
+    
+            int count=1;
+            int j=1;
+            for(int i=1;i<nums.length;i++)
+            {
+                //int j=i;
+    
+                if(nums[i]==nums[i-1])
+                {
+                    count++;
+                }
+                else
+                {
+                    count=1;
+                }
+    
+                if(count<=2)
+                {
+                    nums[j]=nums[i];
+                    j++;
+                }
+    
+            }
+    
+    
+            return j;
+    
+        }
+    }
---
+++ SearchIn2DMatrix.java
@@ -0,0 +1,28 @@
+public class SearchIn2DMatrix {
+        public boolean searchMatrix(int[][] matrix, int target) {
+    
+            int row =matrix.length-1;
+            int col =0;
+    
+    
+            while(row>=0 && col<matrix[0].length)
+            {
+                if(matrix[row][col]==target)
+                {
+                    return true;
+                }
+                else if(matrix[row][col]<target)
+                {
+                    col++;
+                }
+                else
+                {
+                    row--;
+                }
+    
+    
+            }
+            return false;
+    
+        }
+    }
