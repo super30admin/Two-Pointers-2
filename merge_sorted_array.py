f219e3c292ac315e6c5755cf0e99d1a148063aee
Shreya <shreya.cogsci@gmail.com>
2021-09-10 03:21:14 -0500

    Two Pointers Completed

---
+++ merge_sorted_array.py
@@ -0,0 +1,29 @@
+# // Time Complexity : O(n+m) where n is the number of elements
+# // Space Complexity : O(1) 
+# // Did this code successfully run on Leetcode : yes
+# // Any problem you faced while coding this : none
+
+# // Your code here along with comments explaining your approach
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m-1
+        j = n-1
+        k = m+n - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] <= nums2[j]:
+                nums1[k] = nums2[j]
+                j -= 1
+            else:
+                nums1[k] = nums1[i]
+                i -= 1
+            k -= 1
+        
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+        
+        
