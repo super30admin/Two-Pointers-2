0a6fc7fce35f2e24bac020e08b8c29cb87723ba4
panth1921 <panthdev96@gmail.com>
2021-10-12 18:05:13 -0400

    Two-Pointers-2

---
+++ merge.swift
@@ -0,0 +1,32 @@
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :
+// Three line explanation of solution in plain english
+
+// Your code here along with comments explaining your approach
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        if n == 0 || nums1 == nil || nums2 == nil {
+            return 
+        }
+        var i = m-1
+        var j = n-1
+        var k = m+n-1
+        while j >= 0 && i >= 0 {
+            if nums1[i] > nums2[j] {
+                nums1[k] = nums1[i]
+                i -= 1
+                k -= 1
+            } else {
+                nums1[k] = nums2[j]
+                j -= 1 
+                k -= 1
+            }
+        }
+        while j >= 0 {
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+        }
+        
+    }
---
+++ searchMatrix.swift
@@ -0,0 +1,26 @@
+
+// Time Complexity : O(m+n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :
+// Three line explanation of solution in plain english
+
+// Your code here along with comments explaining your approach
+    func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+        if matrix.count == 0 {
+            return false
+        }
+        var m = matrix.count
+        var n = matrix[0].count 
+        var row = 0
+        var col = n - 1
+        while row < m && col >= 0 {
+            if matrix[row][col] > target {
+                col -= 1
+            } else if matrix[row][col] < target {
+                row += 1
+            } else {
+                return true
+            }
+        }
+        return false
+    }
