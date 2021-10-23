d8b01bcef4c0015c78d1a43176735ad5826c3e2d
Nishi Parameshwara <niship007@gmail.com>
2021-10-22 23:02:40 -0400

    Solved

---
+++ MergeSortedArray.py
@@ -0,0 +1,29 @@
+"""
+Time Complexity: O(m+n)
+Space Complecity: O(1)
+"""
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        ptr1 = m - 1
+        ptr2 = n - 1
+        index = m + n - 1
+
+        while (ptr1 >= 0 and ptr2 >= 0):
+            if nums1[ptr1] >= nums2[ptr2]:
+                nums1[index] = nums1[ptr1]
+                ptr1 -= 1
+            elif nums1[ptr1] < nums2[ptr2]:
+                nums1[index] = nums2[ptr2]
+                ptr2 -= 1
+            index -= 1
+
+        while ptr2 >= 0:
+            nums1[index] = nums2[ptr2]
+            ptr2 -= 1
+            index -= 1
+
