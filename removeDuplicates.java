17327a31c1cdba4ddc0ad739091fef726e04c7a2
Jay <dsrujay@gmail.com>
2021-12-19 23:28:04 -0800

    Two-Pointers-2

---
+++ removeDuplicates.java
@@ -0,0 +1,50 @@
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+
+
+class Solution {
+
+    public int removeDuplicates(int[] nums) {
+
+        //
+        // Initialize the counter and the second pointer.
+        //
+        int j = 1, count = 1;
+
+        //
+        // Start from the second element of the array and process
+        // elements one by one.
+        //
+        for (int i = 1; i < nums.length; i++) {
+
+            //
+            // If the current element is a duplicate, increment the count.
+            //
+            if (nums[i] == nums[i - 1]) {
+
+                count++;
+
+            } else {
+
+                //
+                // Reset the count since we encountered a different element
+                // than the previous one.
+                //
+                count = 1;
+            }
+
+            //
+            // For a count <= 2, we copy the element over thus
+            // overwriting the element at index "j" in the array
+            //
+            if (count <= 2) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+}
