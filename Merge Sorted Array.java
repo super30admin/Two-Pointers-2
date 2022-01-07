5b2075b440d4c0af39a18f0d554c01c51adc88f8
Aniket <aniket14.thumar@gmail.com>
2022-01-07 11:17:28 -0800

    Completed Merge Sorted Array Problem

--- Merge Sorted Array.java
+++ Merge Sorted Array.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(m+n)
-// Space Complexity : O(m)
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 
 // Your code here along with comments explaining your approach
@@ -7,31 +7,76 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        // create a array with 'm' length
-        int[] nums1Copy = new int[m];
+        // intialize pointers
+        int i = m - 1;      // nums1
+        int j = n - 1;      // nums2
+        int k = m + n - 1;  // nums1
         
-        // copy all the elements of nums1 to numsCopy array
-        for(int i = 0; i < m; i++){
-            nums1Copy[i] = nums1[i];
+        // traverse reverse through both arrays
+        // insert the maximum element bewteen both arrays at the last position of the 1st array
+        while(i >= 0 && j >= 0){
+                // compare elements
+            if(nums1[i] < nums2[j]){
+                // insert maximum
+                nums1[k] = nums2[j];
+                
+                // decrease the pointer
+                j--;
+                k--;
+            }   
+            else{
+                // insert maximum
+                nums1[k] = nums1[i];
+                
+                // decrease the pointer
+                i--;
+                k--;
+            }     
         }
-        
-        // initialize the pointers from 0
-        int p1 = 0;
-        int p2 = 0;
-        
-        // traverse until all the elements in original nums1 are passed
-        for(int i = 0; i < m + n; i++){
 
-            // check if element at p1 in nums1Copy is less than element at nums2
-            if(p2>=n || (p1 < m && nums1Copy[p1] <  nums2[p2])){
-                // assign the value of nums1Copy's element to nums1 and inrease the p1 pointer
-                nums1[i] = nums1Copy[p1];
-                p1++;
-            } else{
-                // assign the value of nums2's element to nums1 and increase the p2 pointer
-                nums1[i] = nums2[p2];
-                p2++;
-            }
+         // copy the remaining elements from 2nd array to 1st array
+        while(j >= 0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 }
+
+// // Time Complexity : O(m+n)
+// // Space Complexity : O(m)
+// // Did this code successfully run on Leetcode : Yes
+
+// // Your code here along with comments explaining your approach
+
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+//         // create a array with 'm' length
+//         int[] nums1Copy = new int[m];
+        
+//         // copy all the elements of nums1 to numsCopy array
+//         for(int i = 0; i < m; i++){
+//             nums1Copy[i] = nums1[i];
+//         }
+        
+//         // initialize the pointers from 0
+//         int p1 = 0;
+//         int p2 = 0;
+        
+//         // traverse until all the elements in original nums1 are passed
+//         for(int i = 0; i < m + n; i++){
+
+//             // check if element at p1 in nums1Copy is less than element at nums2
+//             if(p2>=n || (p1 < m && nums1Copy[p1] <  nums2[p2])){
+//                 // assign the value of nums1Copy's element to nums1 and inrease the p1 pointer
+//                 nums1[i] = nums1Copy[p1];
+//                 p1++;
+//             } else{
+//                 // assign the value of nums2's element to nums1 and increase the p2 pointer
+//                 nums1[i] = nums2[p2];
+//                 p2++;
+//             }
+//         }
+//     }
+// }
---
+++ Search 2D Matrix.java
@@ -0,0 +1,81 @@
+// Time Complexity : O(m+n) where m = number of rows and n = number of columns
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        if(matrix == null || matrix.length == 0) return false;
+        
+        // find number of rows and columns of input matrix
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        // intialize cooridnates from the top right corner
+        int i = 0;
+        int j = n - 1;
+        
+        // traverse through the matrix
+        while(i < m && j >=0){
+            
+            // if target is found then return true
+            if(matrix[i][j] == target){
+                return true;
+            }
+            
+            // if target is less than current element go to left side i.e decrease column pointer
+            else if(target < matrix[i][j]){
+                j--;            
+            }
+            
+            // else go down and increase the row pointer
+            else{
+                i++;
+            }
+        }
+        
+        
+        // return fasle if target is not found in the matrix
+        return false;
+    }
+}
+
+
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+        
+//         if(matrix == null || matrix.length == 0) return false;
+        
+//         // find number of rows and columns of input matrix
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+        
+//         // intialize cooridnates from the bottom left corner
+//         int i = m - 1;
+//         int j = 0;
+        
+//         //traverse through the matrix
+//         while(i >= 0 && j < n){
+            
+//             // if target is found then return true
+//             if(matrix[i][j] == target) return true;
+            
+//             // if target is less than current element go upside i.e decrease row pointer
+//             else if(target < matrix[i][j]){
+//                     i--;
+//             } 
+            
+//             // else go right and increase the column pointer
+//             else{
+//                     j++;
+//                 }
+//         }
+        
+//         // return fasle if target is not found in the matrix
+//         return false;
+//     }
+// }
+
+
+        
