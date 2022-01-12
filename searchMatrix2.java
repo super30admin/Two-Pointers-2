ea52279261cd50ee4fdd976a0834b230720208b3
Pallavi Krishna Reddy <pallavikrishnareddy.06@gmail.com>
2022-01-11 19:47:43 -0600

    Updating the time and space efficiency

--- searchMatrix2.java
+++ searchMatrix2.java
@@ -1,7 +1,15 @@
+/*
+Time complexity : O(m+n)
+Space complexity: O(1)
+*/
 public class searchMatrix2 {
     public boolean searchMatrix(int[][] matrix, int target) {
         int row = 0;
         int col = matrix[0].length-1;
+        /* traverse from top right corner and if the value is 
+            greater than target, decrease the column else 
+            increment the row.
+        */
         while(col >= 0 && row < matrix.length) {
             if(matrix[row][col] > target) {
                 col--;
