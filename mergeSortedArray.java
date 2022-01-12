2f30a45a73d0d0802a75183874cdd943591b881c
Pallavi Krishna Reddy <pallavikrishnareddy.06@gmail.com>
2022-01-11 19:23:24 -0600

    Merging the 2 sorted arrays

---
+++ mergeSortedArray.java
@@ -0,0 +1,14 @@
+public class mergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m-1;
+        int p2 = n-1;
+        for(int i = m+n-1; i >= 0; i--) {
+            if(p2 < 0) break;
+            if(p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[i] = nums1[p1--];
+            } else {
+                nums1[i] = nums2[p2--];
+            }
+        }
+    }
+}
