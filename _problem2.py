d7c496dd38f6eac57b4f089e960448e9e974faf6
Dharmik <harkhani.d@northeastern.edu>
2022-03-01 12:26:58 -0500

    Problem 1,2 Solved

---
+++ _problem2.py
@@ -0,0 +1,41 @@
+# TC : O(n)
+# SC: O(1)
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        ptr1 = m - 1
+        ptr2 = n - 1
+
+        i = len(nums1) - 1
+
+        while ptr1 >= 0 and ptr2 >= 0:
+            print(ptr1, ptr2, i)
+            if nums1[ptr1] > nums2[ptr2]:
+                nums1[i] = nums1[ptr1]
+                ptr1 -= 1
+            else:
+                nums1[i] = nums2[ptr2]
+                ptr2 -= 1
+
+            i -= 1
+
+        while ptr2 >= 0:
+            nums1[i] = nums2[ptr2]
+            ptr2 -= 1
+
+            i -= 1
+
+        while ptr1 >= 0:
+            nums1[i] = nums1[ptr1]
+            ptr1 -= 1
+
+            i -= 1
+
+        return nums1
+
+
+
