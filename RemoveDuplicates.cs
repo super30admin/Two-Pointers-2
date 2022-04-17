e67e96594f4ea5ac732ff2bf1aeed10264e85e5b
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-04-16 20:11:56 -0700

    Add files via upload

---
+++ RemoveDuplicates.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class RemoveDuplicates
+    {
+        /*
+         *  T.C: O(n) since traversing through whole array 
+         *  S.C: O(1) not using extra space
+         */
+
+        public int RemoveDuplicatesFromArray(int[] nums)
+        {
+
+            if (nums == null || nums.Length == 0)
+                return 0;
+
+            int slow = 1;
+
+            int counter = 1;
+
+            for (int fast = 1; fast < nums.Length; fast++)
+            {
+                if (nums[fast] == nums[fast - 1])
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter = 1;
+                }
+
+                if (counter <= 2)
+                {
+                    nums[slow] = nums[fast];
+                    slow++;
+                }
+            }
+
+            return slow;
+        }
+    }
+}
