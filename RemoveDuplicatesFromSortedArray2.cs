c77897864366ff712437015891b94599d26b5f9c
Rincy Soman <rincysoman@gmail.com>
2022-05-12 23:42:03 -0700

    SearchMatrix2 & RemoveDuplicatesFromSortedArray

---
+++ RemoveDuplicatesFromSortedArray2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Algorithms
+{
+    public class RemoveDuplicatesFromSortedArray2
+    {
+        /// Time Complexity : O(N)
+        // Space Complexity :O(1)
+        // Did this code successfully run on Leetcode :Yes
+        // Any problem you faced while coding this :  Based on the discussion in class
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+
+            int slow = 1;
+
+            int counter = 1;
+            for (int fast = 1; fast < nums.Length; fast++)
+            {
+                if (nums[fast] == nums[fast - 1])
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter = 1;
+                }
+                if (counter <= 2)
+                {
+                    nums[slow++] = nums[fast];
+                }
+
+            }
+            return slow;
+
+        }
+    }
+}
---
+++ SearchMatrix2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Algorithms
+{
+    public class SearchMatrix2
+    {
+        /// Time Complexity : O(M+N)
+        // Space Complexity :O(1)
+        // Did this code successfully run on Leetcode :Yes
+        // Any problem you faced while coding this :  Based on the discussion in class
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            if (matrix == null) return false;
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+
+            int i = 0, j = n - 1;
+
+            while (i <= m - 1 && j >= 0)
+            {
+                if (matrix[i][j] == target) return true;
+
+                if (target > matrix[i][j])
+                {
+                    i++;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+            return false;
+
+        }
+    }
+}
