04fd74d81e981d2a47405354dc42f41fd34c20af
samihan <samihan.jawalkar@gmail.com>
2022-05-16 20:47:50 -0400

    Completed two-pointer-2

---
+++ mergeSortedArray.py
@@ -0,0 +1,24 @@
+'''
+time complexity: O(N)
+n is len of array in which we are merging
+space complexity: O(1)
+'''
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m-1
+        p2 = n-1
+        
+        for i in range(m+n-1,-1,-1):
+            if(p2<0):
+                break
+            if(p1>=0 and nums1[p1]>nums2[p2]):
+                nums1[i]=nums1[p1]
+                p1-=1
+            else:
+                nums1[i]=nums2[p2]
+                p2-=1
+
+        
