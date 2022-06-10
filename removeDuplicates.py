a7a15aa1abf875b3f2584e1d6445699e236feb61
poojitha bijjam <poojitha1589@gmail.com>
2022-06-09 23:04:54 -0500

    Create removeDuplicates.py

---
+++ removeDuplicates.py
@@ -0,0 +1,20 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow = 1
+        count = 1
+        
+        for fast in range(1, len(nums)):
+            if nums[fast] == nums[fast-1]:
+                count += 1
+            else:
+                count = 1
+            if count <= 2:
+                nums[slow] = nums[fast]
+                slow += 1
+        
+        return slow
+                
+            
