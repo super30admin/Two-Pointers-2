db831dc0586325af51048c78a4ff516fc3877daa
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-10 10:11:47 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,39 @@
-
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Three line explanation of solution in plain english
+//****MERGE TWO SORTED ARRAYS****
+// Time Complexity :o(m+n);
+// Space Complexity :o(1);
+// Did this code successfully run on Leetcode :Y
 
 // Your code here along with comments explaining your approach
+
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p=nums1.length-1;
+        int p1=m-1;
+        int p2=n-1;
+        
+        while(p1>=0 && p2>=0)
+        {
+            if(nums1[p1]>nums2[p2])
+            {
+                nums1[p]=nums1[p1];
+                p--;
+                p1--;
+            }
+            else
+            {
+                nums1[p]=nums2[p2];
+                p--;
+                p2--;
+            }
+        }
+        
+        while(p2>=0)
+        {
+           nums1[p]=nums2[p2];
+            p--;
+            p2--; 
+        }
+        
+    }
+}
