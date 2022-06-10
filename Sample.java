02e9ae72bdb3467506dffb1b462ea3e8a8e5b8ce
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-10 11:10:16 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -76,3 +76,36 @@ class Solution {
         
     }
 }
+//****REMOVE DUPLICATES-2****
+//TIME complexity:o(n);
+//SPACE complexity:o(1);
+//Leetcode runnable:Y
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums==null || nums.length==0)
+        {
+            return 0;
+        }
+        int count=1;
+        int slow=1;
+        
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]==nums[i-1])
+            {
+                count++;
+            }
+            else
+            {
+                count=1;
+            }
+            
+            if(count<=2)
+            {
+                nums[slow]=nums[i];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
