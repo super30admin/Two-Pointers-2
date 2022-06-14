4b7195273ed00be2ad7688a85385ceed5fa111a3
STSC <STSC@LENOVO-127795.ad.csueastbay.edu>
2022-06-13 21:55:18 -0700

    Completed

---
+++ searchIn2DMatrix.java
@@ -0,0 +1,20 @@
+// Time Complexity : O(m+n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : No
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length; int n = matrix[0].length;
+        //null
+        if(matrix == null || matrix.length ==0) return false;
+        int i = 0; int j = n-1;
+        while(i < m && j >= 0){
+            if(matrix[i][j] == target) return true;
+            if(matrix[i][j] < target) i++;
+            else j--;
+        }
+        return false;
+    }
+}
