aa594bce44147c4f084c8d7806935d7325aa8cd2
Siddesh2044 <60492570+Siddesh2044@users.noreply.github.com>
2022-06-16 20:05:15 -0400

    Update remove-duplicates-from-sorted-array-ii.java

--- remove-duplicates-from-sorted-array-ii.java
+++ remove-duplicates-from-sorted-array-ii.java
@@ -1,1 +1,29 @@
+//Time Complexity --> O(n) --> Traversing through the array
+//Space Complexity --> O(1)
 
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        int slow = 1;
+        int count = 1;
+        
+        for(int fast = 1; fast < nums.length; fast++) {
+            if(nums[fast] == nums[fast-1]){
+                count++;
+            }
+                
+            else{
+                count = 1;
+            }
+                
+            
+            if(count <= 2){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+                
+        }
+        
+        return slow;
+    }
+}
