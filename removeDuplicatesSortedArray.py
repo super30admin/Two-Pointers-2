f0264272a986103e4c9d76d4d78174986297b827
Jayavani Akkiraju <jayavaniakkiraju@Jayavanis-MacBook-Pro.local>
2022-06-17 12:51:25 +0530

    Two Pointers-2 Completed

--- removeDuplicatesSortedArray.py
+++ removeDuplicatesSortedArray.py
@@ -1,1 +1,43 @@
+'''
+-- Passed test cases on LeetCode
+'''
+class Solution:
+    #O(n) -- Time Complexity ; O(1) - Space Complexity
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
 
+        if (nums == None or len(nums) == 0):
+            return 0
+
+        count = 0
+        slow = 0;
+        fast = 1
+
+        while (fast < len(nums)):
+            if (nums[fast] == nums[slow]):
+                if (count < 1):
+                    count += 1
+                    slow += 1
+                    nums[slow] = nums[fast]
+                    fast += 1
+                else:
+                    fast += 1
+                    while (fast < len(nums) and nums[fast] == nums[fast - 1]):
+                        fast += 1
+            else:
+                if (count == 1):
+                    count = 0
+                    slow += 1
+                    nums[slow] = nums[fast]
+                    fast += 1
+                else:
+                    slow += 1
+                    nums[slow] = nums[fast]
+                    fast += 1
+
+        return slow + 1
+        
+        
--- search2DMatrix.py
+++ search2DMatrix.py
@@ -1,1 +1,22 @@
-
+'''
+-- Passed test cases on LeetCode
+'''
+class Solution:
+    #O(m + n) -- Time Complexity ; O(1) -- Space Complexity
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix) 
+        n = len(matrix[0]) 
+        i = 0
+        j = n - 1 
+        while (i < m and j >=0 ):
+            if target == matrix[i][j]:
+                return True
+            elif target > matrix[i][j]:
+                i += 1
+            else:
+                j -= 1
+        return False
+            
+            
+            
+        
