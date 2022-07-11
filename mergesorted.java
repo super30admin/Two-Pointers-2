ff4adb1f6a632522e86d7699fce3130ad10061ed
prasannaOO7 <70802340+prasannaOO7@users.noreply.github.com>
2022-07-10 20:01:11 -0400

    Update mergesorted.java

--- mergesorted.java
+++ mergesorted.java
@@ -1,4 +1,4 @@
-// Time Complexity : o(m*n)
+// Time Complexity : o(m+n)
 // Space Complexity : o(1)
 // Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this : no
