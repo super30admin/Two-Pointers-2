da1b143cbd872929455b43dc49054157eb83ba5f
saiteja1905 <sjavvaji@asu.edu>
2022-07-11 19:24:12 -0700

    Updated new assignment codes

---
+++ RemoveDuplicates.py
@@ -0,0 +1,16 @@
+from pip import List
+#Time COmplexity = O(n)
+#Space Complexity = O(1)
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        count = 1
+        j = 1     
+        for i in range(1,len(nums)):
+            if(nums[i]==nums[i-1]):
+                count+=1
+            else:
+                count = 1
+            if(count<=2):
+                nums[j] = nums[i]
+                j+=1
+        return j
