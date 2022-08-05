7cbbd7894e51ff7b3d2c5aac06d01b4f82ae298c
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-08-04 21:37:00 -0400

    Create RremoveDuplicates.java

---
+++ RremoveDuplicates.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+       if (nums == null || nums.length==0) return 0;
+        int k=2;
+        int count =1;
+        int start =1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1])
+                count++;
+            else
+                count=1;
+            if(count<=k){
+                nums[start]=nums[i];
+                start++;
+            }
+        }
+        return start;
+    }
+}
