4626812ee3aa5de450f74b14d9710bf65c5e5bdb
SwatiKedarAgarwal <sxa190153@utdallas.edu>
2022-08-04 19:09:21 -0500

    2 Pointers exercise

---
+++ Search2D.java
@@ -0,0 +1,26 @@
+
+// Time Complexity : O(m+n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Three line explanation of solution in plain english
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length-1;
+        int col = 0;
+
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] > target) {
+                row--;
+            } else if (matrix[row][col] < target) {
+                col++;
+            } else { 
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
