6ab64dc3cd1adc2aff9c822cf47b623725094467
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-09-27 18:30:47 -0700

    Update removeDuplicatesII.java

--- removeDuplicatesII.java
+++ removeDuplicatesII.java
@@ -5,21 +5,19 @@
 
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int count = 1; 
-        int i = 1;
+        int count = 1;
         int n = nums.length;
-        while (i < n) {
+        for(int i = 1; i < n; i++){
             if (nums[i] == nums[i - 1]) {
                 count++;
-                if (count > 2) {
-                    removeElement(nums, i);
-                    i--;
-                    n--;
-                }
             } else {
                 count = 1;
             }
-            i++;
+            if (count > 2) {
+                removeElement(nums, i);
+                i--;
+                n--;
+            }
         }
         return n;
     }
