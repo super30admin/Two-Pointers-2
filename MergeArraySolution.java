7e72fb3e63bc7d62efe4f295e88710118e60accf
Sumanth Shankar <sumanth@rezy360.com>
2022-10-01 16:02:28 -0700

    Solution for remove duplicates in sorted array

---
+++ MergeArraySolution.java
@@ -0,0 +1,10 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Three line explanation of solution in plain english
+
+// Your code here along with comments explaining your approach
+
+public class MergeArraySolution {
+    
+}
---
+++ RemoveDuplicatesSolution.java
@@ -0,0 +1,23 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+
+// I will be using 2 pointer approach and a count variable to keep count of repeated numbers.
+public class RemoveDuplicatesSolution {
+    public int removeDuplicates(int[] nums) {
+        int count = 1;
+        int index = 1;
+        for(int i = 1; i < nums.length; i += 1) {
+            if(nums[i - 1] == nums[i]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if(count <= 2) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
