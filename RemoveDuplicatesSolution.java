b6338703506e5c42a443af63a3c07d928a981386
Sumanth Shankar <sumanth@rezy360.com>
2022-10-01 16:40:35 -0700

    Solution for search in 2D matrix

--- RemoveDuplicatesSolution.java
+++ RemoveDuplicatesSolution.java
@@ -5,17 +5,20 @@
 // I will be using 2 pointer approach and a count variable to keep count of repeated numbers.
 public class RemoveDuplicatesSolution {
     public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
         int count = 1;
         int index = 1;
         for(int i = 1; i < nums.length; i += 1) {
             if(nums[i - 1] == nums[i]) {
-                count++;
+                count += 1;
             } else {
                 count = 1;
             }
             if(count <= 2) {
                 nums[index] = nums[i];
-                index++;
+                index += 1;
             }
         }
         return index;
--- Search2DMatrixSolution.java
+++ Search2DMatrixSolution.java
@@ -1,10 +1,30 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Three line explanation of solution in plain english
+// Time Complexity : O(m + n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
 
-// Your code here along with comments explaining your approach
+// Compare the target with first row last column element in the matrix.
+// If target is less move towards right if not move towards down.
 
 public class Search2DMatrixSolution {
-    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0) {
+            return false;
+        }
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int row = 0;
+        int col = n - 1;
+        
+        while(row < m && col >= 0) {
+            if(matrix[row][col] < target) {
+                row += 1;
+            } else if(matrix[row][col] > target) {
+                col -= 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
