dbaf72cbe3dba915681539edb47e07bdd93aa3ca
Yash Shirodkar <yashshirodkar@yashs-mbp.mynetworksettings.com>
2023-01-06 18:44:28 -0500

    completed sprint

---
+++ Search a 2D Matrix II.java
@@ -0,0 +1,23 @@
+// Time Complexity = O(m+n)
+// Space Complexity = O(1)
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = 0;
+        int j = n -1;
+        while(i<m && j >=0){
+            System.out.println(matrix[i][j]);
+            if (matrix[i][j] == target) return true;
+            
+            if(matrix[i][j] > target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+    }
+}
