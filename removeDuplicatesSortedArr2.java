c7c112d3ad3b10ef9b585150173e7694087e4de7
rutujamandlikFT <123407038+rutujamandlikFT@users.noreply.github.com>
2023-02-11 15:55:01 -0500

    Create removeDuplicatesSortedArr2.java

---
+++ removeDuplicatesSortedArr2.java
@@ -0,0 +1,22 @@
+//TC:O(n)
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int count = 1;
+        int slow = 1;
+        
+        for(int i=1; i<nums.length;i++){
+            if(nums[i]<= nums[i-1]){
+                count++;
+            }else{
+                count = 1;
+            }
+            
+            if(count <= 2){
+                nums[slow] = nums[i];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
