d6ce217c22612430393f64c7df38874c97a15fa2
NAVEEN US <88378808+usnaveen@users.noreply.github.com>
2023-02-14 21:12:02 +0530

    Create mergeSortedarray.py

---
+++ mergeSortedarray.py
@@ -0,0 +1,17 @@
+# Author: Naveen US
+# Titile: Merge sorted array.
+
+# Time complexity: O(m+n)
+# Space complexity: O(1)
+# Did the code run on leetcode: Yes
+
+def merge(self, nums1, m, nums2, n):
+        while m > 0 and n > 0:
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        if n > 0:
+            nums1[:n] = nums2[:n]
