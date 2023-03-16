cfc26382d35d7cbd2ddafd2f4c1e9678364366c4
Sriharsha2799 <spodichenapally@albany.edu>
2023-03-16 11:07:50 -0400

    Two Pointers-2 completed

---
+++ MergeSortedArray.java
@@ -0,0 +1,32 @@
+/*the time complexity of the implementation below is O(m+n)*/
+import java.util.Arrays;
+
+class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+
+        MergeSortedArray solution = new MergeSortedArray();
+        solution.merge(nums1, m, nums2, n);
+
+        System.out.println(Arrays.toString(nums1));
+    }
+
+}
