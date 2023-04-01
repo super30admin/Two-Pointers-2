f756744d65300de54a036922ad69dd6c426bbb5e
krish-navulla <navullavamshi2423@gmail.com>
2023-04-01 00:57:05 -0400

    completed

---
+++ Rem_dup_fsorted_2.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+    
+        
+        n = len(nums)
+        
+        if n < 3:
+            return n
+        
+        i , j = 1, 2
+        
+        while j < n:
+            if nums[i-1] != nums[j]:
+                i += 1
+            
+            
+            nums[i] = nums[j]
+            j+= 1
+  
+        return i+1
