815b50cb36717fb3b5a39265d100cda76ded852a
Apurva Kemkar <apurvakemkar@Apurvas-MBP.lan>
2023-05-02 13:03:15 -0500

    Two-Pointers-2 completed. SearchA2dMatrix2, RemoveDupsSortedArr2 and MergeSortedArray. Please review, Thank You.

--- RemoveDupsSortedArr2.java
+++ RemoveDupsSortedArr2.java
@@ -1,10 +1,35 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
 // Three line explanation of solution in plain english
 
 // Your code here along with comments explaining your approach
+/*
+ * As we want to solve this in-place we start i and j pointers from index 1 and count = 1. i will always move forward and j will move forward only when
+ * count is valid(<=2) we copy value from i as i always moves, in j and j++. If current i == nums[i-1] we increment count or 
+ * set to 1 as array is sorted it means we have new element. 
+ */
 
 public class RemoveDupsSortedArr2 {
-    
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length < 1) {
+            return 0;
+        }
+
+        int j = 1;
+        int count = 1;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == nums[i - 1]) {
+                count++;
+            }
+            else {
+                count = 1;
+            }
+            if(count <= 2) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
 }
--- SearchA2dMatrixii.java
+++ SearchA2dMatrixii.java
@@ -1,10 +1,36 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(m + n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
 // Three line explanation of solution in plain english
 
 // Your code here along with comments explaining your approach
+/*
+ * We saw brute force for checking each element in matrix with O(mn) time complexity. Then as rows and coloumns are sorted;
+ * we thought of Binary Search. 1 BS on row mlogn time, 2. BS on col nlogm time or 3. BS on both row and col will have issue. As arrays are sorted we think of TWO POINTERS.
+ * We start from row = 0 and col = n - 1 or col = 0 and row = m - 1 because from that position we can decide definitely correct direction to move pointers.
+ * if num > target decrease high go left else go down till low and high are in bounds.
+ */
 
 public class SearchA2dMatrixii {
-    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length < 1) {
+            return false;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = 0;
+        int high = n - 1;
+        while(low < m && high >= 0) {
+            if(matrix[low][high] == target) {
+                return true;
+            }
+            else if(matrix[low][high] > target) {
+                high--;
+            }
+            else {
+                low++;
+            }
+        }
+        return false;
+    }
 }
