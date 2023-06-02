726eac4fa8d6fe177ede2d6b785e40c5bb748357
PRAYAG-PATEL-1998 <90693541+PRAYAG-PATEL-1998@users.noreply.github.com>
2023-06-01 17:37:28 -0700

    Update Sample.java
    Two-pointers-2 completed

--- Sample.java
+++ Sample.java
@@ -1,7 +1,69 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        //if(nums.length == 1)return 0;
+        int i = 1;
+        int j = 1;
+        int count = 1;
+        while(j < nums.length){
+            if(nums[j] == nums[j-1]){
+                count++;
+                
+            }else{
+                count =1;
+            }
+            if(count <= 2){
+                nums[i] = nums[j];
+                i++;
+            }
+            j++;
+                
+        }
+        return i;
+    }
+}
 
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Three line explanation of solution in plain english
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(m==0 && n==0)return;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n -1;
+        
+        while(i>=0 && j>=0){
+            if(nums1[i] < nums2[j]){
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            }else{
+                nums1[k] = nums1[i];
+                i--;
+                k--;
+            }
+        }
+        while(j>=0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+        
+    }
+}
 
-// Your code here along with comments explaining your approach
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int row = 0;
+        int col = n-1;
+        while(row<m && col>=0){
+            if(matrix[row][col] < target){
+                row++;
+            }else if(matrix[row][col] > target){
+                col--;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
