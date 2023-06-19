5c4d9997e34b6f6f8fc5f915098df6758544cbd7
amreenmahenaz <113376911+amreenmahenaz@users.noreply.github.com>
2023-06-19 15:03:29 -0700

    Delete meregsortedarray.py

--- meregsortedarray.py
+++
@@ -1,9 +0,0 @@
-# // Time Complexity :O(N)
-# // Space Complexity :O(1)
-# // Did this code successfully run on Leetcode :Yes
-# // Any problem you faced while coding this :no
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        for i in range(n):
-           nums1[i+m]=nums2[i]
-        return nums1.sort()
