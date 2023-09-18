65279e64b67bcf314aab91a32462c6b9776ed1f2
UmangThakur15 <umangthakuremail@gmail.com>
2023-09-18 16:18:22 -0400

    MergeSortedArrayBruteForce

---
+++ MergeSortedArrayBruteForce.java
@@ -0,0 +1,73 @@
+class MergeSortedArrayBruteForce {
+    //Using Brute Force storing values in result matrix
+    //TC will be O(m + n(log (m+n)))
+    //SC will be O(1)
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(nums1 == null || nums1.length==0){   //base Condn
+            return;
+        }
+
+        if(nums2 == null || nums2.length==0){
+            return;
+        }
+
+        int i=0;        //pointer for nums1 array.
+        int j=0;        //pointer for nums2 array.
+        int k=0;        // pointer for sorted result array.
+        int[] result = new int[m+n]; //array to store stored elements
+
+
+        while(i<m && j<n)       //travering through nums1 and nums2
+        {
+            if(nums1[i]<nums2[j])
+            {
+                result[k]=nums1[i];
+                i++;
+            }
+            else {
+                result[k]=nums2[j];
+                j++;
+            }
+            k++;
+        }
+
+
+        while(i<m)//if any value left in nums1
+        {
+            result[k] = nums1[i];
+            k++;
+            i++;
+        }
+
+
+        while(j<n)  //if any value left in nums2
+        {
+            result[k] = nums2[j];
+            k++;
+            j++;
+        }
+
+
+        //to copy all the elements in nums array.
+        for(int r=0;r<nums1.length;r++)
+        {
+            nums1[r] = result[r];
+        }
+
+        for(int num: nums1){
+            System.out.println(num);
+        }
+
+    }
+
+    public static void main(String[] args){
+        MergeSortedArrayBruteForce obj= new MergeSortedArrayBruteForce();
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+
+        obj.merge(nums1,m,nums2,n);
+    }
+}
---
+++ out/production/Two-Pointers-2/README.md
@@ -0,0 +1,10 @@
+# Two-Pointers-2
+
+## Problem1 (https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/)
+
+
+## Problem2 (https://leetcode.com/problems/merge-sorted-array/)
+
+
+## Problem3 (https://leetcode.com/problems/search-a-2d-matrix-ii/)
+
