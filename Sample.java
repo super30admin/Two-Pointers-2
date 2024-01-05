5521f662dcf7f4e198e7937413a4f3930bd2d1b7
Tanmay Singal <tanmaysingal@icloud.com>
2024-01-04 20:26:23 -0800

    Merge Sorted Array.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,36 @@
 
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Three line explanation of solution in plain english
+// Time Complexity : O(m+n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Three line explanation of solution in plain English
+// Two pointers to iterate arrays in reverse and one to keep track of where to input in nums1
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        if(m < 1) {
+            for(int i = 0; i < n; i++){
+                nums1[i] = nums2[i];
+            }
+            return;
+        }
+
+        int a = m-1;
+        int b = n-1;
+        int insert = nums1.length - 1;
+
+        while(a >= 0 || b >= 0){
+            int numa = a > -1 ? nums1[a] : Integer.MIN_VALUE;
+            int numb = b > -1 ? nums2[b] : Integer.MIN_VALUE;
+            if(numa > numb){
+                nums1[insert--] = numa;
+                a--;
+            } else {
+                nums1[insert--] = numb;
+                b--;
+            }
+        }
+
+    }
+}
