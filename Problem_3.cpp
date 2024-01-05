1d459e3785d70a845e6f397e35b47c9ffa91462b
God7wyn <jamesgodwynj@gmail.com>
2024-01-05 02:21:42 -0500

    Updated Code

--- Problem_3.cpp
+++ Problem_3.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     bool searchMatrix(std::vector<std::vector<int>>& matrix, int target) {
-        int n = matrix.size() - 1;
+        int n = matrix.size();
 
         int i = 0, j = matrix[0].size() - 1;
         while (i < n && j >= 0) {
