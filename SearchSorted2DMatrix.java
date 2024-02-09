6a7d57d648964fe81887fb8ae122b0a4117228a6
Amanjot Singh <amanjsingh@Amanjots-MacBook-Air.local>
2024-02-09 13:00:53 -0400

    Search 2D Sorted Matrix

---
+++ SearchSorted2DMatrix.java
@@ -0,0 +1,19 @@
+public class SearchSorted2DMatrix {
+    class Solution {
+        public boolean searchMatrix(int[][] matrix, int target) {
+            int row = 0;
+            int col = matrix[row].length - 1;
+            while (row < matrix.length && col >= 0) {
+                if (matrix[row][col] == target) {
+                    return true;
+                }
+                if (matrix[row][col] > target) {
+                    col--;
+                } else {
+                    row++;
+                }
+            }
+            return false;
+        }
+    }
+}
